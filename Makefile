5309914aa191d0bf1d33e7e4f58cc4101e0ccf18
Meng Xipeng <mengxipeng@gmail.com>
2023-08-29 18:30:10 +0800

    add code to print VuAssetFactory mpInterface

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@
 all:./modinfos/libmousebot.ts
 	./node_modules/frida-compile/bin/compile.js -o _agent.js index.ts
 
-./modinfos/libmousebot.ts:./libs/arm64-v8a/libmousebot.so
+./modinfos/libmousebot.ts:
 	make -C jni
-	./utils/so2ts.py -b $^ -o $@
+	./utils/so2ts.py -b ./libs/arm64-v8a/libmousebot.so -o $@
 
 clean:
 	rm _agent.js ./modinfos/lib*.ts
--- jni/mousebot.cc
+++ jni/mousebot.cc
@@ -1,16 +1,39 @@
 
 
+#include <stdio.h>
+
 extern "C" {
     void _frida_log(const char* p);
     void _frida_hexdump(void* p, unsigned int i);
 }
 
 
+//  log stuff
+#define LOG_INFOS_WITH_N(N, fmt, args...)                         \
+do{                                                               \
+    char buff[N];                                                 \
+    snprintf(buff, N,  fmt , ##args);                             \
+    _frida_log(buff);                                    \
+}while(0)
+
+#define LOG_INFOS_WITH_N_FILE_LINE(N, fmt, args...)               \
+LOG_INFOS_WITH_N(N, "[%s:%d] " fmt , __FILE__, __LINE__,  ##args);
+
+#define LOG_INFOS(fmt, args...)  LOG_INFOS_WITH_N_FILE_LINE(0x800, fmt, ##args)
+
+#define LOG_HEXDUMP(p, n) do {_frida_hexdump((void*)(p),n);} while(0)
+
+
+////////////////////////////////////////////////////////////////////////////////
+// stuff from libmain.so 
+
+struct VuAssetFactory{
+    static VuAssetFactory* mpInterface;
+}; 
 
 extern "C" int test(void* base){
 
-    _frida_log("Hello from C++ !");
-    _frida_hexdump(base, 0x20);
+    LOG_INFOS(" VuAssetFactory mpInterface %p", VuAssetFactory::mpInterface);
 
     return 0; 
 }
